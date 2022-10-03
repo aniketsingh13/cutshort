0659ee3ad67db64d6d1915114398f9c086ce411f
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-10-03 17:02:46 +0530

    feat: progressbar

--- onboardingform/src/component/form/Form.jsx
+++ onboardingform/src/component/form/Form.jsx
@@ -1,15 +1,47 @@
-import React from 'react';
-import "./Form.css"
+import React from "react";
+import { useState } from "react";
+import "./Form.css";
 
 const Form = () => {
+  const [page, setPage] = useState(0);
+
+  const formTitles = [
+    {
+      main: "Welcome! First things First...",
+      sub: "You can always change them later.",
+    },
+    {
+      main: "Let's set up a home for all your work",
+      sub: "You can always create another workspace later",
+    },
+    {
+      main: "How are you planning to use Eden?",
+      sub: "We'll streamline your setup experience accordingly.",
+    },
+    { },
+  ];
+
   return (
-    <div className='form_container'>
-      <div className='header'>
-        <img src='https://res.cloudinary.com/aniket-singh/image/upload/v1664780523/Images/logo1_qykdvg.svg' alt="heading" className='header_img' />
-        <h3 className='header_text'>Eden</h3>
+    <div className="form_container">
+      <div className="header">
+        <img
+          src="https://res.cloudinary.com/aniket-singh/image/upload/v1664780523/Images/logo1_qykdvg.svg"
+          alt="heading"
+          className="header_img"
+        />
+        <h3 className="header_text">Eden</h3>
+      </div>
+      <div className="progressbar">
+        <div></div>
+      </div>
+      <div className="formCard">
+        <div>
+        <div className="formCard_headerMain">{formTitles[page].main}</div>
+        <div className="formCard_headerSub">{formTitles[page].sub}</div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
