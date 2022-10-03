2abad254a4c28c01ad93f9ab3e2f32e681adab83
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-10-03 13:40:51 +0530

    add form header

--- onboardingform/src/App.js
+++ onboardingform/src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Form from './component/form/Form';
+
 
 function App() {
   return (
     <div className="App">
-     
+    <Form />
     </div>
   );
 }
---
+++ onboardingform/src/component/form/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./Form.css"
+
+const Form = () => {
+  return (
+    <div className='form_container'>
+      <div className='header'>
+        <img src='https://res.cloudinary.com/aniket-singh/image/upload/v1664780523/Images/logo1_qykdvg.svg' alt="heading" className='header_img' />
+        <h3 className='header_text'>Eden</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Form
